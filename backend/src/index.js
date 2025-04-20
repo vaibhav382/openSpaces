a7fd69126d105935faeae00b144d7b8293a62d3b
vaibhav <vaibhavpandey382@gmail.com>
2025-04-21 02:25:41 +0530

    message functionality added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js"
-import router from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 import cookieParser from "cookie-parser"
 
 const app = express()
@@ -9,7 +11,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth",router)
+app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 dotenv.config()
 
